dbddd1042882628ac00012792d2a0755ec0a9d9f
Raul Bayramov <raul.bairamovi633@hum.tsu.edu.ge>
2024-12-31 00:13:36 +0400

    Fixed some buges(Searchbar, Features)

--- js_files/shares/header.js
+++ js_files/shares/header.js
@@ -5,6 +5,11 @@ export function header() {
     const logo = document.querySelector(".logo");
     const searchContainer = document.querySelector(".search-container");
     const cart = document.querySelector(".right-section");
+    const toogleBtnIcon = document.querySelector(".toggle_btn i");
+    const dropDownMenu = document.querySelector(".dropdown_menu");
+    const searchInput = document.querySelector('.search-input');
+
+    //change places of elements when width <=992px
     function rearangeElements() {
         const links = document.querySelector(".links");
         if (window.innerWidth <= 992) {
@@ -14,50 +19,44 @@ export function header() {
         else {
             navbar.insertBefore(logo, links);
         }
+
     }
-    const toogleBtnIcon = document.querySelector(".toggle_btn i");
-    const dropDownMenu = document.querySelector(".dropdown_menu");
+    window.addEventListener("load", rearangeElements);
+    window.addEventListener("resize", rearangeElements);
+
+    //when clicking toggle button change it width other symbol(= ->'X')
     toogleBtn.onclick = function () {
         dropDownMenu.classList.toggle("open");
         const isOpen = dropDownMenu.classList.contains("open");
         toogleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
     }
-    window.addEventListener("load", rearangeElements);
-    window.addEventListener("resize", rearangeElements);
 
     // search-button:
-    const searchInput = document.querySelector('.search-input');
-    searchInput.style.width = (window.innerWidth * 0.2) + 'px'
+    searchContainer.style.width = (window.innerWidth * 0.2) + 'px'
     function updateSearchInputWidth() {
         if (window.innerWidth <= 576) {
-            searchInput.style.width = (window.innerWidth * 0.2) + 'px';
+            searchContainer.style.width = (window.innerWidth * 0.2) + 'px';
         }
         else {
-            searchInput.style.width = (window.innerWidth * 0.25) + 'px';  // Pencere genişliğinin %20'si
+            searchContainer.style.width = (window.innerWidth * 0.25) + 'px';
         }
-
     }
-    searchContainer.addEventListener('mouseover', () => {
+    searchContainer.addEventListener('click', (event) => {
         if (window.innerWidth <= 992) {
+            const screenWidth = window.innerWidth;
+            searchContainer.style.width = screenWidth / 2 + 'px';
             logo.style.display = "none";
-            toogleBtn.style.marginRight = "1rem"
-            cart.style.marginLeft = "1rem"
-            searchInput.style.width = (window.innerWidth * 0.3) + 'px';  // Pencere genişliğinin %50'si
-
         }
     })
-    searchContainer.addEventListener('mouseout', (button) => {
-        if (window.innerWidth <= 992) {
-            logo.style.display = "block";
-            logo.style.marginRight = "1rem"
-            searchInput.style.width = (window.innerWidth * 0.2) + 'px';
-            updateSearchInputWidth();
-        }
+    searchContainer.addEventListener('mouseout', () => {
+        logo.style.display = "block";
+        updateSearchInputWidth()
     })
     window.addEventListener('load', updateSearchInputWidth);
-
-    // Pencere boyutu değiştiğinde (responsive)
     window.addEventListener('resize', updateSearchInputWidth);
+
+
+    //when clicking login button open login page
     document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".login_button").addEventListener("click", function () {
             window.location.href = "page_of_login.html";
